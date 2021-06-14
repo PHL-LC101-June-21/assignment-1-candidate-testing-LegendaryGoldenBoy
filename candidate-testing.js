e59a44e7c68ae36bc2689dacc97fd0703e5e16e7
youngcoder21 <70248579+LegendaryGoldenBoy@users.noreply.github.com>
2021-06-14 21:13:20 +0000

    added my solution

--- candidate-testing.js
+++ candidate-testing.js
@@ -15,13 +15,14 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+  
 let candidateName = input.question("What is your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
-const candidateAnswer = input.question(question);
+let candidateAnswer = input.question(question);
 
 }
 
