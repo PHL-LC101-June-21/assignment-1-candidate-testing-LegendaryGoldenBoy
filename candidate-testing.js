5d725c86753b90da523eb08ea7d93f29d6ad0715
youngcoder21 <70248579+LegendaryGoldenBoy@users.noreply.github.com>
2021-06-21 18:40:57 +0000

    I inputted my solution.

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,34 +8,40 @@ let candidateName = "";
 let question = ("Who was the first American woman in space? ");
 let correctAnswer = ("Sally Ride");
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ['Who was the first American woman in space? ',
+'True or false: 5 kilometer == 5000 meters? ',
+'(5+3)/2*10 = ? ',
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+'What is the minimum crew size for the ISS? '];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
   
-candidateName = input.question("What is your name: ");
+  candidateName = input.question("What is your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+  
+  for (let i = 0; i <= questions.length; i++) {
+    candidateAnswers[i] = input.question(questions[i]);
+    
+  }  
 
-candidateAnswer = input.question(question);
+
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer) {
-  console.log("Your answer is correct. ");
-}else {
-  console.log("Your answer is not correct. ");
-}
+  console.log(`You have answered ${candidateAnswers} and the correct answers are ${correctAnswers}`);
 
-  let grade;
+
+  let grade; 
   
 
   return grade;
